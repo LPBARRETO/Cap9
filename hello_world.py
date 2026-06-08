fdd3d41c513ad92d573f5e40c02f2a5ad24a4c66
Lucas barreto <lpbarreto97@gmail.com>
2026-06-08 01:08:57 -0300

    Add Hello World Flask

--- hello_world.py
+++ hello_world.py
@@ -8,5 +8,5 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    # Roda o servidor localmente na porta 5000
-    app.run(debug=True)
+    # Roda na porta 5000
+    app.run(port=5000, debug=True)
